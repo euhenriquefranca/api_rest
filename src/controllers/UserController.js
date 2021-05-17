186fef0dfe342c521081a2e42b20018c4d044b98
Henrique França <euhenriquefranca@gmail.com>
2021-05-17 09:25:53 -0300

    Middleware of security for users

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -14,6 +14,8 @@ class UserController {
   async index(req, res) {
     try {
       const users = await User.findAll();
+      console.log('USER ID', req.userId);
+      console.log('USER Email', req.userEmail);
       return res.json(users);
     } catch (e) {
       return res.json(null);
