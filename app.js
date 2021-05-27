3e12596d42a36be81c2f024dc38d77b3bb922cd9
Henrique França <euhenriquefranca@gmail.com>
2021-05-26 22:40:40 -0300

    Routes of the student

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
-import './src/database';
-import express from 'express';
-import homeRoutes from './src/routes/homeRoutes';
-import userRoutes from './src/routes/userRoutes';
-import tokenRoutes from './src/routes/tokenRoutes';
+import "./src/database";
+import express from "express";
+import homeRoutes from "./src/routes/homeRoutes";
+import userRoutes from "./src/routes/userRoutes";
+import tokenRoutes from "./src/routes/tokenRoutes";
+import alunoRoutes from "./src/routes/alunoRoutes";
 
 class App {
   constructor() {
@@ -21,9 +22,10 @@ class App {
   }
 
   routes() {
-    this.app.use('/', homeRoutes);
-    this.app.use('/users/', userRoutes);
-    this.app.use('/tokens/', tokenRoutes);
+    this.app.use("/", homeRoutes);
+    this.app.use("/users/", userRoutes);
+    this.app.use("/tokens/", tokenRoutes);
+    this.app.use("/alunos/", alunoRoutes);
   }
 }
 
