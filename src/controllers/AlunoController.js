e64c70f7462e38ab36cea1312593d2da773ba100
Henrique França <euhenriquefranca@gmail.com>
2021-05-26 23:17:40 -0300

    CRUD of the students

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -5,6 +5,84 @@ class AlunoController {
     const alunos = await Aluno.findAll();
     res.json(alunos);
   }
+
+  async store(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ["Missing Id"],
+        });
+      }
+      const aluno = await Aluno.findByPk(id);
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ["Student does not exist"],
+        });
+      }
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ["Missing Id"],
+        });
+      }
+      const aluno = await Aluno.findByPk(id);
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ["Student does not exist"],
+        });
+      }
+      await aluno.destroy();
+      return res.json({
+        deleted: true,
+      });
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ["Missing Id"],
+        });
+      }
+      const aluno = await Aluno.findByPk(id);
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ["Student does not exist"],
+        });
+      }
+      const alunoAtualizado = await aluno.update(req.body);
+      return res.json(alunoAtualizado);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
 }
 
 export default new AlunoController();
